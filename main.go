7473004b0c4a4f10e68a781e6ee3c24f0b84e6f4
chonlatee <jumratsee@gmail.com>
2023-01-28 22:02:25 +0700

    add ping path

--- main.go
+++ main.go
@@ -11,8 +11,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello\n")
 }
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	log.Printf("INFO: %+v\n", r.Header)
+	fmt.Fprintf(w, "pong\n")
+}
+
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/ping", ping)
 	log.Printf("INFO: server start on port: 8000\n")
 	http.ListenAndServe(":8000", nil)
 }
